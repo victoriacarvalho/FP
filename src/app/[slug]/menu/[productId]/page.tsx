d35c256f0a75a24ed06d1fa2ceb2faf8b00166e4
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-03-14 13:59:29 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 
@@ -10,23 +11,31 @@ interface ProductPageProps{
 }
 
 const ProductPage = async ({params} : ProductPageProps) => {
-
-    
-    const {slug, productId} = await params;
-    const product = await db.product.findUnique({where: {id: productId}});
+  
+    const {slug, productId } = await params;
+    const product = await db.product.findUnique({where: {id: productId}, include:{
+      restaurant: {
+        select:{
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        }
+      }
+    }});
+   
     if (!product){
         return notFound()
     }
-    return (
-    <>
-    <ProductHeader product={product}></ProductHeader>
-        <h1>
-            ProductPage
-        </h1>
-        {slug}
-        {productId}
+    if(product.restaurant.slug.toUpperCase() != slug)
+    {
+      return notFound();
+    }
 
-        </>
+    return (
+      <div className="flex h-full flex-col">
+        <ProductHeader product={product}/>
+        <ProductDetails product={product}/>
+      </div>
     );
 }
  
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { FormatCurrency } from "@/helpers/format-currency";
+
 
 interface ProductsProps {
     products : Product[]
@@ -21,10 +23,7 @@ const Products = ({products} : ProductsProps) => {
                             {product.description}
                         </p>
                         <p className="pt-3 text-sm font-semibold">
-                            {new Intl.NumberFormat("pt-BR",{
-                            style: 'currency',
-                            currency: 'BRL'
-                            }).format(product.price)} </p>
+                            {FormatCurrency(product.price)} </p>
                     </div>
                     <div className="relative">
                         <Image
