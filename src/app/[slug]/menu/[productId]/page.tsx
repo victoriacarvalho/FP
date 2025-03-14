4f3514ce98684eb51d203833dc7dd8170a49179f
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-03-14 14:03:36 -0300

    fix: bug slug upercase

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -26,7 +26,7 @@ const ProductPage = async ({params} : ProductPageProps) => {
     if (!product){
         return notFound()
     }
-    if(product.restaurant.slug.toUpperCase() != slug)
+    if(product.restaurant.slug.toUpperCase() != slug.toUpperCase())
     {
       return notFound();
     }
